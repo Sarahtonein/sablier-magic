5d5662c3682ce62efb84f43b4b165d1f9d231a10
Sarahtonein <joemoe1899@gmail.com>
2023-12-09 18:28:01 +1000

    added gas  calc into its own script

---
+++ gas.js
@@ -0,0 +1,18 @@
+const { ethers } = require('ethers');
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_PROVIDER);
+
+
+async function getGasPrice() {
+  try {
+    const gasPrice = await provider.getGasPrice();
+    return gasPrice;
+  } catch (error) {
+    console.error('Error getting gas price:', error.message);
+    throw error;
+  }
+}
+
+// Export function
+module.exports = {
+    getGasPrice,
+};
